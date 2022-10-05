00f253397eeb8d39d0df202c1dcea4fc3d9f0e98
jodi106 <jonas@dierenbach.ch>
2022-10-05 20:37:27 +0300

    update ap_scenario

--- scripts/scenario_editor/api_scenario.py
+++ scripts/scenario_editor/api_scenario.py
@@ -1,14 +1,50 @@
+import carla 
+import random 
+import pandas as pd
+
 def setup_world():
-    world=
-
-def spawn_vehicle():
     pass
 
-def transform_coordinates():
-    pass
+def spawn_entities(world, transform_list, entity_type):
+    bp_lib = world.get_blueprint_library() 
+  
+    entity_list = []
+    for transform in transform_list:
+        if entity_type == "c":
+            entity_bp = random.choice(bp_lib.filter('vehicle'))
+        elif entity_type == "p":
+            entity_bp = random.choice(bp_lib.filter('passenger'))
+        entity =  world.try_spawn_actor(entity_bp, transform)
+        entity_list.append(entity) 
 
-def control_dummy():
-    pass
+    return entity_list
 
-def start_autopilot():
-    pass
+def transform_coordinates(coords_list):
+    return transform_list
+
+def read_gui_input(path):
+    df = pd.read_csv(path)   
+
+    return df
+
+def control_dummy_car(vehicle_list):
+    for car in vehicle_list:
+        car.apply_control(carla.VehicleControl(throttle = 0.5))
+
+def control_dummy_passenger(passenger_list):
+    for passenger in passenger_list:
+        passenger.apply_control(carla.WalkerControl(speed = 0.55))
+
+def start_autopilot(vehicle):
+    vehicle.set_autopilot(True)
+
+
+# Connect to the client and get the world object
+client = carla.Client('localhost', 2000) 
+world = client.get_world() 
+
+
+
+
+# Set the all vehicles in motion using the Traffic Manager
+
