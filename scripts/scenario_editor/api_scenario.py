be553cbb2fd2e1d24d31e708ee048793d2525c3e
Stefan Stingl <stfnstingl@gmail.com>
2022-10-05 19:38:07 +0200

    Setup_World

--- scripts/scenario_editor/api_scenario.py
+++ scripts/scenario_editor/api_scenario.py
@@ -1,5 +1,24 @@
+# Import the CARLA Python API library and some utils
+import carla 
+import math 
+import random 
+import time 
+
 def setup_world():
-    world=
+
+    client = carla.Client('localhost', 2000) 
+    world = client.get_world() 
+
+    #Check if Town 4 is already loaded, if not load it
+    if world.get_map().name != "Carla/Maps/Town04":
+        client.set_timeout(15.0) #BC mostly takes longer than default 5 Seconds
+        client.load_world("Town04")
+
+        #Setting Spectator Camera Position to the 4-Way Intersection
+        spectator = world.get_spectator()
+        spectator.set_location(carla.Location(x = 255, y=-173, z=40))
+
+        
 
 def spawn_vehicle():
     pass
@@ -12,3 +31,7 @@ def control_dummy():
 
 def start_autopilot():
     pass
+
+
+setup_world()
+
