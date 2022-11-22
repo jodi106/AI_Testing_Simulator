87c1f88964ba2ae3d2b2cc74990f6c37ae701029
Stungler <47352800+StayFN@users.noreply.github.com>
2022-11-22 13:03:59 +0100

    More Enums

--- Unity/Assets/Entities/Waypoint.cs
+++ Unity/Assets/Entities/Waypoint.cs
@@ -17,8 +17,8 @@ namespace Entity
 
         public int Id { get; set; }
         public Location Position { get; set; }
-        public ActionType ActionTypeInfo { get; set; }
-        public string Priority { get; set; }
+        public ActionType ActionTypeInfo { get; set; } 
+        public string Priority { get; set; } // has enum: PriorityType
         public List<TriggerInfo> TriggerList { get; set; }
         // One Waypoint can have mutliple triggers for an event
 
