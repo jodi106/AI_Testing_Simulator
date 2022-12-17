4bea412bcbb062fa77ce531a3780c46ee42c8085
Felix Krömer <kroemer.felix@gmail.com>
2022-12-17 19:02:47 +0100

    add stubs for Ego vehicle controller

---
+++ Unity/Assets/Scripts/ViewControllers/EgoViewController.cs
@@ -0,0 +1,72 @@
+﻿using Assets.Enums;
+using Entity;
+using UnityEngine;
+
+public class EgoViewController : VehicleViewController, IBaseEntityController
+{
+    private Ego ego;
+    public new void Awake()
+    {
+        base.Awake();
+
+        var egoPosition = new Location(transform.position.x, transform.position.y, 0, 0);
+        this.ego = new Ego(egoPosition);
+        this.ego.setView(this);
+
+        EventManager.StartListening(typeof(CancelPathSelectionAction), x =>
+        {
+            expectingAction = false;
+        });
+
+        EventManager.StartListening(typeof(MouseClickAction), x =>
+        {
+            if (!placed)
+            {
+                placed = true;
+                sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1);
+                //mainController.setEgoVehicle(this.ego);
+            }
+        });
+    }
+
+    public override void triggerActionSelection()
+    {
+        expectingAction = true;
+    }
+
+    public void submitDestination(Location destination)
+    {
+        expectingAction = false;
+    }
+
+    public override bool hasAction()
+    {
+        return false;
+    }
+
+    public new void destroy()
+    {
+        base.destroy();
+    }
+
+    public override void deleteAction()
+    {
+    }
+
+    public override void setColor(Color color)
+    {
+        if (placed)
+        {
+            this.sprite.color = color;
+        }
+        else
+        {
+            this.sprite.color = new Color(color.r, color.g, color.b, 0.5f);
+        }
+    }
+
+    public override BaseEntity getEntity()
+    {
+        return this.ego;
+    }
+}
