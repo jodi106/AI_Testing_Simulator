7f44f0a590dcbf613b8d4760fad900b278553f86
jodi106 <jonas@dierenbach.ch>
2023-01-16 21:37:50 +0100

    runnerTool v0.1

--- scripts/runnerTool/runnerTool.py
+++ scripts/runnerTool/runnerTool.py
@@ -4,47 +4,59 @@ import time
 import os
 import sys
 import py_trees
-
-os.environ["CARLA_ROOT"] = 'C:/CARLA/WindowsNoEditor'
-os.environ["SCENARIO_RUNNER_ROOT"] = "C:/Users/jonas/OneDrive - bwedu/Studium_Master/2_HWS_2022-23/AI Testing Simulator/code/AI_Testing_Simulator/scripts/runnerTool"
-sys.path.append(os.getenv("CARLA_ROOT")+"/PythonAPI/carla/dist/carla-0.9.13.egg")
-sys.path.append(os.getenv("CARLA_ROOT")+"/PythonAPI/carla/agents")
-sys.path.append(os.getenv("CARLA_ROOT")+"/PythonAPI/carla")
-
-import scenario_runner
 from argparse import Namespace
 
-cmd = "C:\\CARLA\\WindowsNoEditor\\CarlaUE4.exe"
+#------------------------- SET INPUTS --------------------------------------------------------------------------------#
 
-process = subprocess.Popen(cmd)
-time.sleep(10)
+PATH_TO_CARLA_ROOT = "C:/CARLA/WindowsNoEditor"
+PATH_TO_SCENARIO_RUNNER_ROOT = "C:/Users/jonas/OneDrive - bwedu/Studium_Master/2_HWS_2022-23/AI Testing Simulator/code/AI_Testing_Simulator/scripts/runnerTool"
+PATH_TO_XOSC_FILES = "C:/Users/jonas/OneDrive - bwedu/Studium_Master/2_HWS_2022-23/AI Testing Simulator/code/AI_Testing_Simulator/scripts/runnerTool/scenarios"
+CARLA_VERSION = "0.9.13"
 
-client = carla.Client('localhost', 2000) 
-world = client.load_world('Town04')
-
-
-def run_multiple_scenarios(openscenario):
-
-    
-
-    """     spectator = world.get_spectator()
-    spectator.set_location(carla.Location(x = 255, y=-173, z=40)) """
-
-    args = Namespace(additionalScenario='', agent=None, agentConfig='', configFile='', debug=False, file=False, 
-                host='127.0.0.1', json=False, junit=False, list=False, openscenario=openscenario, 
-                openscenarioparams=None, output=False, outputDir='', port='2000', randomize=False, record='', reloadWorld=True, repetitions=1, route=None, scenario=None, 
-                sync=False, timeout='10.0', trafficManagerPort='8000', trafficManagerSeed='0', waitForEgo=False)
+#---------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------#
+os.environ["CARLA_ROOT"] = PATH_TO_CARLA_ROOT
+os.environ["SCENARIO_RUNNER_ROOT"] = PATH_TO_SCENARIO_RUNNER_ROOT
+sys.path.append(os.getenv("CARLA_ROOT")+"/PythonAPI/carla/dist/carla-" + CARLA_VERSION + ".egg")
+sys.path.append(os.getenv("CARLA_ROOT")+"/PythonAPI/carla/agents")
+sys.path.append(os.getenv("CARLA_ROOT")+"/PythonAPI/carla")
+import scenario_runner
+#---------------------------------------------------------------------------------------------------------------------#
 
 
+class RunnerTool(object):
 
-    scenario_runner.main(args)
-    
+    def __init__(self):
+        pass
 
-scenarios = ['C:\\CARLA\\scenario_runner-master\\srunner\\examples\\OurStartStopLC.xosc','C:\\CARLA\\scenario_runner-master\\srunner\\examples\\OurStartStopLC.xosc']
+    def start_carla(self):
+        self.carla_exe = subprocess.Popen(PATH_TO_CARLA_ROOT+"/CarlaUE4.exe")
+        time.sleep(10)
+        client = carla.Client('localhost', 2000) 
+        world = client.load_world('Town04')
 
-for scenario in scenarios:
-    print("current dir::")
-    print(dir())
-    print(py_trees.blackboard.Blackboard().__str__())
-    run_multiple_scenarios(scenario)
-    print(py_trees.blackboard.Blackboard().__str__())
+        # implement error caching #
+
+    def run_multiple_scenarios(self):
+
+        folder_addr = PATH_TO_XOSC_FILES
+        self.file_list = os.listdir(folder_addr)
+
+        for file in self.file_list:
+            if ".xosc" in file:
+                openscenario = folder_addr + "\\" + file
+
+                args = Namespace(additionalScenario='', agent=None, agentConfig='', configFile='', debug=False, file=False, 
+                            host='127.0.0.1', json=False, junit=False, list=False, openscenario=openscenario, 
+                            openscenarioparams=None, output=False, outputDir='', port='2000', randomize=False, record='', reloadWorld=True, repetitions=1, route=None, scenario=None, 
+                            sync=False, timeout='10.0', trafficManagerPort='8000', trafficManagerSeed='0', waitForEgo=False)
+
+                scenario_runner.main(args)
+        
+def main():
+    runner_tool = RunnerTool()
+    runner_tool.start_carla()
+    runner_tool.run_multiple_scenarios()
+
+if __name__ == "__main__":
+    sys.exit(main())
