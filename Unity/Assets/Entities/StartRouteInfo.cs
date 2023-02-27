b6e43dd5490c044a3f93d071fa115435b482b7f7
Natalie <701natalie@gmail.com>
2023-02-27 16:31:21 +0100

    fix

---
+++ Unity/Assets/Entities/StartRouteInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class StartRouteInfo
+    {
+
+        public Vehicle vehicle { get; set; }
+        public Location locationCarla { get; private set; }
+
+        public StartRouteInfo(Vehicle vehicle, Waypoint waypoint)
+        {
+            this.vehicle = vehicle;
+            this.locationCarla = waypoint.Location;
+            //this.locationCarla = CalculateLocationCarla(waypoint.Location);
+        }
+
+        public StartRouteInfo(Vehicle vehicle, Location location)
+        {
+            this.vehicle = vehicle;
+            this.locationCarla = location;
+            //this.locationCarla = CalculateLocationCarla(location);
+        }
+
+        private Location CalculateLocationCarla(Location pos)
+        {
+            (float xCarla, float yCarla) = SnapController.UnityToCarla(pos.X, pos.Y);
+            float rotCarla = SnapController.UnityRotToRadians(pos.Rot);
+            rotCarla = (float)Math.Round(rotCarla * 100f) / 100f; // otherwise we'll have a number like this 3.339028E-05
+            return new Location(xCarla, yCarla, 0.3f, rotCarla);
+        }
+    }
+}
