89f1becf5a12034d982f3d9f1fef49b97942fc76
Stungler <stfnstingl@gmail.com>
2023-03-06 19:04:55 +0100

    Fixed Merge Error and Added Serialized Classes

---
+++ Unity/Assets/Helpers/ColorSer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Helpers
+{
+    //Color Class partly compatible with UnityEngine.Color, that is Serializable
+    public class ColorSer
+    {
+
+    }
+}
