0441d752fdb0c7d80def882fa3b308ca94b08a4e
Eva <eva.beha@web.de>
2023-05-02 12:07:24 +0200

    Debugging for  intersection generation

--- xodrExport/OpenDriveXMLGenerator/OpenDriveXMLGenerator/OpenDriveXMLBuilder.cs
+++ xodrExport/OpenDriveXMLGenerator/OpenDriveXMLGenerator/OpenDriveXMLBuilder.cs
@@ -38,7 +38,7 @@ namespace OpenDriveXMLGenerator
             userDataElement.AddVectorSceneElement(program: "RoadRunner", version: "2019.2.12 (build 5161c15)");
         }
 
-        public XODRRoad AddStraightRoad(float startX = 0 , float startY = 0, float hdg = 0.0f, float length = 0.0f, bool crossing = false, float crossingLength = 0.0f, float crossingWidth = 0.0f){      
+        public XODRRoad AddStraightRoad(float startX = 0 , float startY = 0, double hdg = 0.0, float length = 0.0f, bool crossing = false, float crossingLength = 0.0f, float crossingWidth = 0.0f){      
 
             var road = RootElement.AddRoadElement(
                 name: "Road " + id.ToString(), 
@@ -107,125 +107,112 @@ namespace OpenDriveXMLGenerator
             return road;
         }
 
-        public XODRRoad AddCurve(float startX = 0 , float startY = 0, float hdg = 0.0f, float length = 0.0f, float curvature = 0.0)
+        
+        public XODRRoad AddLeftCurveToIntersection(float startX = 0, float startY = 0, double arc = 0, double hdg,  int predecessorId, int successorId)
         {
-            var road = RootElement.AddRoadElement(
-                name: "Road " + id.ToString(), 
-                length: length.ToString(),
+            var curve = RootElement.AddRoadElement(
+                name: "Road " + id.ToString(),
+                length: "7.8539816339744830e+00",
                 id: id.ToString(),
                 junction: "-1");
-
             id++;
-                var plainView = road.AddPlainViewElement();
-                    var geometry1 = plainView.AddGeometryElement(
-                        s: "0.0",
-                        x: startX.ToString(),
-                        y: startY.ToString(),
-                        hdg: hdg.ToString(),
-                        length: length.ToString());
 
-                        var arc = road.AddArcElement(road, curvature.ToString());
-            
-            var lanes = road.AddLanesElement();
-                var laneSection = lanes.AddLaneSectionElement(s:"0");
+            var link = AddLinkElement(curve);
+                var predecessor = link.AddPredecessor(link, "road", predecessorId.ToString());
+                var successor = link.AddSuccessor(link, "road", successorId.ToString());
+
+            var plainView = curve.AddPlainViewElement();
+            var geometry = plainView.AddGeometryElement(
+                s: "0.0",
+                x: startX.ToString(),
+                y: startY.ToString(),
+                hdg: hdg.ToString(),
+                length: "7.8539816339744830e+00");
+                var arc = geometry.AddArcElement(geometry, arc.ToString());
+
+            var lanes = curve.AddLanesElement();
+                var laneSection = lanes.AddLaneSectionElement(s: "0");
                     var left = laneSection.AddDirectionElement(Direction.Left);
-                        var lane = left.AddLaneElement(id:"1", type:"driving", level:"false");
-                            lane.AddLinkElement();
-                            lane.AddWidthElement();
+                        var laneLeft = left.AddLaneElement(id: "1", type: "driving", level: "false");
+                            laneLeft.AddLinkElement();
+                            laneLeft.AddWidthElement();
                     var center = laneSection.AddDirectionElement(Direction.Center);
-                        var laneCenter = center.AddLaneElement(id:"0", type:"none", level:"false");
+                        var laneCenter = center.AddLaneElement(id: "0", type: "none", level: "false");
                             laneCenter.AddLinkElement();
-                            laneCenter.AddWidthElement(a:"0");
-                    var right = laneSection.AddDirectionElement(Direction.Right);
-                        var laneRight = right.AddLaneElement(id:"-1", type:"driving", level:"false");
-                            laneRight.AddLinkElement();
-                            laneRight.AddWidthElement();
-        }
+                            laneCenter.AddWidthElement(a: "0");
 
-        public XODRRoad AddCurveToIntersection(float startX = 0, float startY = 0, float arc = 0, float hdg, bool isIntersection = false, XODRRoad incomingRoad1, int incomingRoad2)
-        {
-            var curve1 = RootElement.AddRoadElement(
-                name: "Road " + id.ToString(),
-                length: "7.8539816339744830e+00",
-                id: id.ToString(),
-                junction: "1");
-            id++;
-
-            var link1 = AddLinkElement(curve1);
-                var predecessor1 = AddPredecessor(link1, "road", incomingRoad1.toString());
-                var successor1 = AddSuccessor(link1, "road", incomingRoad2.toString());
-
-            var plainView1 = curve1.AddPlainViewElement();
-            var geometry1 = plainView1.AddGeometryElement(
-                s: "0.0",
-                x: startX.ToString(),
-                y: startY.ToString(),
-                hdg: hdg.toString(),
-                length: length.ToString());
-                var arc1 = geometry1.AddArcElement(geometry1, arc.toString());
-
-            var lanes1 = curve1.AddLanesElement();
-                var laneSection1 = lanes1.AddLaneSectionElement(s: "0");
-                    var left1 = laneSection1.AddDirectionElement(Direction.Left);
-                        var laneLeft1 = left1.AddLaneElement(id: "1", type: "driving", level: "false");
-                            laneLeft1.AddLinkElement();
-                            laneLeft1.AddWidthElement();
-                    var center1 = laneSection1.AddDirectionElement(Direction.Center);
-                        var laneCenter1 = center1.AddLaneElement(id: "0", type: "none", level: "false");
-                            laneCenter1.AddLinkElement();
-                            laneCenter1.AddWidthElement(a: "0");
-                    var right1 = laneSection2.AddDirectionElement(Direction.Right);
-
-
-            var curve2 = RootElement.AddRoadElement(
-                name: "Road " + id.ToString(),
-                length: "7.8539816339744830e+00",
-                id: id.ToString(),
-                junction: "1");
-            id++;
-
-            var link2 = AddLinkElement(curve2);
-                var predecessor2 = AddPredecessor(link2, "road", incomingRoad2.toString());
-                var successor2 = AddSuccessor(link2, "road", incomingRoad1.toString());
-
-            var plainView2 = curve2.AddPlainViewElement();
-            var geometry2 = plainView2.AddGeometryElement(
-                s: "0.0",
-                x: startX.ToString(),
-                y: startY.ToString(),
-                hdg: hdg.toString(),
-                length: length.ToString());
-                var arc2 = geometry1.AddArcElement(geometry1, arc.toString());
-
-            var lanes2 = curve2.AddLanesElement();
-                var laneSection2 = lanes2.AddLaneSectionElement(s: "0");
-                    var left2 = laneSection2.AddDirectionElement(Direction.Left);
-                    var center2 = laneSection2.AddDirectionElement(Direction.Center);
-                        var laneCenter2 = center2.AddLaneElement(id: "0", type: "none", level: "false");
-                            laneCenter2.AddLinkElement();
-                            laneCenter2.AddWidthElement(a: "0");
-                    var right2 = laneSection2.AddDirectionElement(Direction.Right);
-                        var laneRight2 = right2.AddLaneElement(id: "-1", type: "driving", level: "false");
-                            laneRight2.AddLinkElement();
-                            laneRight2.AddWidthElement();
+            return curve;
             
         }
+
+        public XODRRoad AddRightCurveToIntersection(float startX = 0, float startY = 0, double arc = 0, double hdg = 0, int predecessorId, int successorId)
+        {
+            var curve = RootElement.AddRoadElement(
+               name: "Road " + id.ToString(),
+               length: "7.8539816339744830e+00",
+               id: id.ToString(),
+               junction: "-1");
+            id++;
+
+            var link = AddLinkElement(curve);
+            var predecessor = link.AddPredecessor(link, "road", predecessorId.toString());
+            var successor = link.AddSuccessor(link, "road", successorId.toString());
+
+            var plainView = curve.AddPlainViewElement();
+            var geometry = plainView.AddGeometryElement(
+                s: "0.0",
+                x: startX.ToString(),
+                y: startY.ToString(),
+                hdg: hdg.toString(),
+                length: "7.8539816339744830e+00");
+            var arc = geometry.AddArcElement(geometry, arc.ToString());
+
+            var lanes = curve.AddLanesElement();
+            var laneSection = lanes.AddLaneSectionElement(s: "0");
+            var center = laneSection.AddDirectionElement(Direction.Center);
+            var laneCenter = center.AddLaneElement(id: "0", type: "none", level: "false");
+            laneCenter.AddLinkElement();
+            laneCenter.AddWidthElement(a: "0");
+            var right = laneSection.AddDirectionElement(Direction.Right);
+            var laneRight = right.AddLaneElement(id: "-1", type: "driving", level: "false");
+            laneRight.AddLinkElement();
+            laneRight.AddWidthElement();
+
+            return curve;
+
+        }
+
 
         public XODRJunction Add3wayIntersection(float startX = 0, float startY = 0)
         {
             float length = 30;
 
-            var incomingRoad1 = this.AddStraightRoad(startX: startX, startY: startY, hdg: 0, lenght: length, false);
+            var incomingRoadId1 = id;
+            var incomingRoad1 = this.AddStraightRoad(startX, startY, 0, length, false);
 
-            var incomingRoad2 = this.AddStraightRoad(startX + 35, startY - 35, 1.5707963267949, length, false);
+            float startX2 = startX + 35;
+            float startY2 = startX - 35;
+            var incomingRoadId2 = id;
+            var incomingRoad2 = this.AddStraightRoad(startX2, startY2, 1.5707963267949, length, false);
 
-            var incomingRoad3 = this.AddStraightRoad(startX + 35, startY + 5, 1.5707963267949, length, false);
+            float startX3 = startX;
+            float startY3 = startY + 5;
+            var incomingRoadId3 = id;
+            var incomingRoad3 = this.AddStraightRoad(startX3, startY3, 1.5707963267949, length, false);
 
-            var curve1 = this.AddCurveToIntersection(startX + 30, startY, -0.2, 0.0, incomingRoad1, incomingRoad2);
+            float startXCurve1 = startX + 35;
+            float startYCurve1 = startY - 5;
+            var curve1Left = this.AddLeftCurveToIntersection(startXCurve1, startYCurve1, 0.2, 1.5707963267949, incomingRoadId1, incomingRoadId2);
+            var curve1Right = this.AddRightCurveToIntersection(startXCurve1, startYCurve1, 0.2, 1.5707963267949, incomingRoadId2, incomingRoadId1);
 
-            var curve2 = this.AddCurveToIntersection(startX + 30, startY, 0.2, 0.0, incomingRoad2, incomingRoad3);
+            float startXCurve2 = startX + 30;
+            float startYCurve2 = startY;
+            var curve2Left = this.AddLeftCurveToIntersection(startXCurve2, startYCurve2, 0.2, 0.0, incomingRoadId1, incomingRoadId2);
+            var curve2Right = this.AddRightCurveToIntersection(startXCurve2, startYCurve2, 0.2, 0, incomingRoadId2, incomingRoadId1);
 
-            var connectionRoad1 = this.AddStraightRoad(startX + 35, startY - 5, 1.5707963267949, 10, false);
+            float startX4 = startX + 35;
+            float startY4 = startX - 5;
+            var connectionRoad1 = this.AddStraightRoad(startX4, startY4, 1.5707963267949, 10, false);
 
             return junction;
         }
@@ -234,25 +221,51 @@ namespace OpenDriveXMLGenerator
         {
             float length = 30;
 
+            var incomingRoadId1 = id;
             var incomingRoad1 = this.AddStraightRoad(startX, startY, 0, length, false);
 
-            var incomingRoad2 = this.AddStraightRoad(startX + 35, startY - 35, 1.5707963267949, length, false);
+            float startX2 = startX + 35;
+            float startY2 = startX - 35;
+            var incomingRoadId2 = id;
+            var incomingRoad2 = this.AddStraightRoad(startX2, startY2, 1.5707963267949, length, false);
 
-            var incomingRoad3 = this.AddStraightRoad(startX + 35, startY + 5, 1.5707963267949, length, false);
+            float startX3 = startX;
+            float startY3 = startY + 5;
+            var incomingRoadId3 = id;
+            var incomingRoad3 = this.AddStraightRoad(startX3, startY3, 1.5707963267949, length, false);
 
-            var incomingRoad4 = this.AddStraightRoad(startX + 40, startY, 0, length, false);
+            float startX4 = startX + 40;
+            float startY4 = startY;
+            var incomingRoadId4 = id;
+            var incomingRoad4 = this.AddStraightRoad(startX4, startY4, 0, length, false);
 
-            var curve1 = this.AddCurveToIntersection(startX + 30, startY, -0.2, 0.0, incomingRoad1.id, incomingRoad2.id);
+            float startXCurve1 = startX + 35;
+            float startYCurve1 = startY - 5;
+            var curve1Left = this.AddLeftCurveToIntersection(startXCurve1, startYCurve1, 0.2, 1.5707963267949, incomingRoadId1, incomingRoadId2);
+            var curve1Right = this.AddRightCurveToIntersection(startXCurve1, startYCurve1, 0.2, 1.5707963267949, incomingRoadId2, incomingRoadId1);
 
-            var curve2 = this.AddCurveToIntersection(startX + 30, startY, 0.2, 0.0, incomingRoad2.id, incomingRoad3.id);
+            float startXCurve2 = startX + 30;
+            float startYCurve2 = startY;
+            var curve2Left = this.AddLeftCurveToIntersection(startXCurve2, startYCurve2, 0.2, 0, incomingRoadId1, incomingRoadId2);
+            var curve2Right = this.AddRightCurveToIntersection(startXCurve2, startYCurve2, 0.2, 0, incomingRoadId2, incomingRoadId1);
 
-            var curve3 = this.AddCurveToIntersection(startX + 30, startY, -0.2, 3.1415926535898, incomingRoad2.id, incomingRoad3.id);
+            float startXCurve3 = startX + 35;
+            float startYCurve3 = startY + 5;
+            var curve3Left = this.AddLeftCurveToIntersection(startXCurve3, startYCurve3, 0.2, 4.7123889803846899, incomingRoadId4, incomingRoadId3);
+            var curve3Right = this.AddRightCurveToIntersection(startXCurve3, startYCurve3, 0.2, 4.7123889803846899, incomingRoadId3, incomingRoadId4);
 
-            var curve4 = this.AddCurveToIntersection(startX + 30, startY, 0.2, 3.1415926535898, incomingRoad2.id, incomingRoad3.id);
+            float startXCurve4 = startX + 40;
+            float startYCurve4 = startY;
+            var curve4Left = this.AddLeftCurveToIntersection(startXCurve4, startYCurve4, 0.2, 3.1415926535898, incomingRoadId4, incomingRoadId2);
+            var curve4Right = this.AddRightCurveToIntersection(startXCurve4, startYCurve4, 0.2, 3.1415926535898, incomingRoadId2, incomingRoadId4);
 
-            var connectionRoad1 = this.AddStraightRoad(startX + 35, startY - 5, 0, 10, false);
+            float startX5 = startX + 30;
+            float startY5 = startY;
+            var connectionRoad1 = this.AddStraightRoad(startX5, startY5, 0, 10, false);
 
-            var connectionRoad2 = this.AddStraightRoad(startX + 30, startY, 1.5707963267949, 10, false);
+            float startX6 = startX + 35;
+            float startY6 = startY - 5;
+            var connectionRoad2 = this.AddStraightRoad(startX6, startY6, 1.5707963267949, 10, false);
 
             return junction;
         }
