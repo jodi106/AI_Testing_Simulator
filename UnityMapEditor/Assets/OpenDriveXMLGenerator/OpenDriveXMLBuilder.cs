ecfdbf8ff2cd270c4edcd49151e50fb63ca58200
Eva <eva.beha@web.de>
2023-07-26 15:32:13 +0200

    Adapted lane width of roundabouts to 4

--- UnityMapEditor/Assets/OpenDriveXMLGenerator/OpenDriveXMLBuilder.cs
+++ UnityMapEditor/Assets/OpenDriveXMLGenerator/OpenDriveXMLBuilder.cs
@@ -1378,7 +1378,7 @@ namespace OpenDriveXMLGenerator
                 }
             }
 
-            laneRight.AddWidthElement();
+            laneRight.AddWidthElement(a: "3.5000000000000000e+00", b: "0.0000000000000000e+00", c: "2.4842518299085264e-02", d: "-2.1313586653776513e-03");
             var laneRight2 = right.AddLaneElement(id: "-2", type: "driving", level: "false");
             laneRight2.AddLinkElement();
             laneRight2.AddWidthElement(a: "1.5");
@@ -1505,8 +1505,7 @@ namespace OpenDriveXMLGenerator
                 }
             }
 
-            laneLeft.AddWidthElement();
-            laneLeft.AddWidthElement();
+            laneLeft.AddWidthElement(a: "3.500000000000000e+00", b:"0.0000000000000000e+00", c: "2.4842518299085271e-02", d: "-2.1313586653776521e-03");
             var laneLeft2 = left.AddLaneElement(id: "2", type: "driving", level: "false");
             laneLeft2.AddLinkElement();
             laneLeft2.AddWidthElement(a: "1.5");
@@ -1516,7 +1515,7 @@ namespace OpenDriveXMLGenerator
 
         //The custom road has no left lane or left sidewalk
         public XODRRoad AddRightLaneCurve(XODRJunction junction, float startX = 0, float startY = 0, float hdg = 0,
-            float length = 0, string laneWidth = "3.5", string curvature = null, bool sidewalk = false,
+            float length = 0, string laneWidth = "4", string curvature = null, bool sidewalk = false,
             SequenceInfo predecessorInfo = null, SequenceInfo successorInfo = null)
         {
             if (predecessorInfo != null)
